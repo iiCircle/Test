e4d947cdc19fc2ac34a7113c7ba9ef58bdc00ed3
Circle <chentengfei@bilibili.com>
2020-05-22 17:04:39 +0800

    commit

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 echo "start ..."
 
-python3 -m airtest run /data/untitled.air --device Android://127.0.0.1:5037/10.23.22.54:7589 --log /data/untitled.air/log/
+# python3 -m airtest run /data/untitled.air --device Android://127.0.0.1:5037/10.23.22.54:7589 --log /data/untitled.air/log/
 
 echo "end ..."
