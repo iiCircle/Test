6c48649104a64694148ab7bbf54933cd13c1ab5d
Circle <chentengfei@bilibili.com>
2020-05-22 15:33:38 +0800

    commit

--- build.sh
+++ build.sh
@@ -2,6 +2,4 @@
 
 echo "start ..."
 
-echo ${PLUGIN_TEST1}
-echo $USERNAME
-echo $PASSWORD
+python3 -m airtest run /data/untitled.air --device Android://127.0.0.1:5037/10.23.22.54:7589 --log /data/untitled.air/log/
